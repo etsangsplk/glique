722d475dc40c8ecd27ea140baf578d2f3216fe9a
Bill Mill <bill.mill@gmail.com>
2013-10-29 22:15:36 -0400

    test neighbors

--- glique_test.go
+++ glique_test.go
@@ -60,3 +60,43 @@ func TestRemNode(t *testing.T) {
 		t.Fatalf("The edge 1,2 should have been completely eliminated")
 	}
 }
+
+func TestNeighbors(t *testing.T) {
+	g := NewGraph()
+	i := 1
+	g.AddNode(i)
+
+	ns, err := g.Neighbors(i)
+	if err != nil {
+		t.Fatalf("Should not have found an err: %s", err)
+	}
+	if len(ns) > 0 {
+		t.Fatalf("i should have no neighbors")
+	}
+
+	g.AddNode(2)
+	g.AddNode(3)
+	g.AddEdge(1,2)
+	g.AddEdge(2,3)
+
+	ns, err = g.Neighbors(i)
+	if err != nil {
+		t.Fatalf("Should not have found an err: %s", err)
+	}
+	if len(ns) != 1 || ns[0] != 2 {
+		t.Fatalf("i should have exactly one neighbor, got %#v", ns)
+	}
+
+	g.AddNode(2)
+	g.AddEdge(1,3)
+
+	ns, err = g.Neighbors(i)
+	if err != nil {
+		t.Fatalf("Should not have found an err: %s", err)
+	}
+	if len(ns) != 2 ||
+		(ns[0] != 2 && ns[0] != 3) ||
+		(ns[1] != 2 && ns[1] != 3) {
+		t.Fatalf("i should have exactly two neighbors 2 and 3, got %#v", ns)
+	}
+}
